7aa9689123fc814fd2d9fe09eb1a544d0933f422
Aleksandra Lasiewicka <alex.lasiewicka@gmail.com>
2024-06-04 08:28:27 +0200

    Checking if the user enters just one character, and checking if it is a letter.

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ import sys
 
 print("Hello in hangman game!")
 lives = 10
-word = "Kamil"
+word = "kamil"
 user_word = []
 used_letters = []
 
@@ -27,31 +27,35 @@ for _ in word:
     user_word.append("_")
 
 while True:
-    letter = input("Podaj literę: ")
+    letter = input("Podaj literę: ").lower()
     if letter.isalpha():
-        used_letters.append(letter)
-        found_indexes = find_indexes(word, letter)
-        if len(found_indexes) == 0:
-            print("Wrong letter!")
-            lives -= 1
+        if len(letter)==1:
+            used_letters.append(letter)
+            found_indexes = find_indexes(word, letter)
+            if len(found_indexes) == 0:
+                print("Wrong letter!")
+                lives -= 1
 
+            else:
+                for index in found_indexes:
+                    user_word[index] = letter
+
+                if "".join(user_word) == word:
+                    print("YOU WIN! ")
+                    sys.exit(0)
+
+            if lives < 0:
+                print("YOU LOST :(")
+                sys.exit(0) #kończenie programu bez żadnego błędu
+            state_of_game()
         else:
-            for index in found_indexes:
-                user_word[index] = letter
-
-            if "".join(user_word) == word:
-                print("YOU WIN! ")
-                sys.exit(0)
-
-        if lives < 0:
-            print("YOU LOST :(")
-            sys.exit(0) #kończenie programu bez żadnego błędu
-        state_of_game()
+            print("Only one letter please...")
     else:
         print("Only letters...")
 
 #Do zrobienia:
 # Walidacja wprowadzonych danych, cyfr, znaków specjalnych, etc, zamiana liter na małe też by się przydała
+# walidacja czy użytkownik wprowadził tylko jedną literę
 # użytkownik nie powinien drugi raz wpisać tej samej litery, za drugim razem powinniśmy go o tym poinformować
 # lista słów, zamiast jednego słowa i losować kolejne słowo do odgadnięcia
 # może zewnętrzne Api do słów? pamiętanie które słowa już padły w grze
